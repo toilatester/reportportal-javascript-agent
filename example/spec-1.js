f5ec6fa8bb166fb9cc1df1c2f18e75757994b3dd
minhhoang <minhhoang@kms-technology.com>
2018-12-10 22:38:34 +0700

    Update debug

--- example/spec-1.js
+++ example/spec-1.js
@@ -6,23 +6,43 @@ describe('Protractor Demo App Spec-1', function() {
     browser.ignoreSynchronization = true;
     browser.get('https://www.google.com');
   });
-  // it('should create data successfully Spec 1', function() {
-  //   log.logInfo('Spec 1 Testing Log4js');
-  //   expect(3).toEqual(3);
-  // });
+  it('should create data successfully Spec 1', function() {
+    console.log('should create data successfully Spec 1 1');
+    log.logInfo('should create data successfully Spec 1 1');
+    log.logInfo('should create data successfully Spec 1 2');
+    log.logInfo('should create data successfully Spec 1 3');
+    log.logInfo('should create data successfully Spec 1 4');
+    log.logInfo('should create data successfully Spec 1 5');
+    expect(3).toEqual(3);
+  });
 
-  // it('should save data successfully Spec 1', function() {
-  //   log.logInfo('Spec 1 Testing Log4js');
-  //   expect(3).toEqual(323);
-  // });
+  it('should save data successfully Spec 1', function() {
+    console.log('should save data successfully Spec 1 1');
+    log.logInfo('should save data successfully Spec 1 1');
+    log.logInfo('should save data successfully Spec 1 2');
+    log.logInfo('should save data successfully Spec 1 3');
+    log.logInfo('should save data successfully Spec 1 4');
+    log.logInfo('should save data successfully Spec 1 5');
+    expect(3).toEqual(323);
+  });
 
-  // it('should edit data successfully Spec 1', function() {
-  //   log.logInfo('Spec 1 Testing Log4js');
-  //   expect(3).toEqual(3);
-  // });
+  it('should edit data successfully Spec 1', function() {
+    console.log('should edit data successfully Spec 1 1');
+    log.logInfo('should edit data successfully Spec 1 1');
+    log.logInfo('should edit data successfully Spec 1 2');
+    log.logInfo('should edit data successfully Spec 1 3');
+    log.logInfo('should edit data successfully Spec 1 4');
+    log.logInfo('should edit data successfully Spec 1 5');
+    expect(3).toEqual(3);
+  });
 
   it('should update data successfully Spec 1', function() {
-    log.logInfo('Spec 1 Testing Log4js');
+    console.log('should update data successfully Spec 1 1');
+    log.logInfo('should update data successfully Spec 1 1');
+    log.logInfo('should update data successfully Spec 1 2');
+    log.logInfo('should update data successfully Spec 1 3');
+    log.logInfo('should update data successfully Spec 1 4');
+    log.logInfo('should update data successfully Spec 1 5');
     expect(3).toEqual(323);
   });
 });
--- example/spec-2.js
+++ example/spec-2.js
@@ -6,24 +6,44 @@ describe('Protractor Demo App Spec-2', function() {
     browser.ignoreSynchronization = true;
     browser.get('https://github.com/toilatester/reportportal-javascript-agent');
   });
-
   it('should create data successfully Spec 2', function() {
-    log.logInfo('Spec 2 Testing Log4js');
+    console.log('should create data successfully Spec 2 1');
+    log.logInfo('should create data successfully Spec 2 1');
+    log.logInfo('should create data successfully Spec 2 2');
+    log.logInfo('should create data successfully Spec 2 3');
+    log.logInfo('should create data successfully Spec 2 4');
+    log.logInfo('should create data successfully Spec 2 5');
+    expect(3).toEqual(13);
+  });
+
+  it('should save data successfully Spec 2', function() {
+    console.log('should save data successfully Spec 2 1');
+    log.logInfo('should save data successfully Spec 2 1');
+    log.logInfo('should save data successfully Spec 2 2');
+    log.logInfo('should save data successfully Spec 2 3');
+    log.logInfo('should save data successfully Spec 2 4');
+    log.logInfo('should save data successfully Spec 2 5');
+    expect(3).toEqual(323);
+  });
+
+  it('should edit data successfully Spec 2', function() {
+    console.log('should edit data successfully Spec 2 1');
+    log.logInfo('should edit data successfully Spec 2 1');
+    log.logInfo('should edit data successfully Spec 2 2');
+    log.logInfo('should edit data successfully Spec 2 3');
+    log.logInfo('should edit data successfully Spec 2 4');
+    log.logInfo('should edit data successfully Spec 2 5');
     expect(3).toEqual(3);
   });
 
-  // it('should update data successfully Spec 2', function() {
-  //   log.logInfo('Spec 1 Testing Log4js');
-  //   expect(13).toEqual(131);
-  // });
-
-  // it('should edit data successfully Spec 2', function() {
-  //   log.logInfo('Spec 1 Testing Log4js');
-  //   expect(13).toEqual(313);
-  // });
-
-  // it('should delete data successfully Spec 2', function() {
-  //   log.logInfo('Spec 1 Testing Log4js');
-  //   expect(3).toEqual(13);
-  // });
+  it('should update data successfully Spec 2', function() {
+    console.log('should update data successfully Spec 2 1');
+    log.logInfo('should update data successfully Spec 2 1');
+    log.logInfo('should update data successfully Spec 2 1');
+    log.logInfo('should update data successfully Spec 2 2');
+    log.logInfo('should update data successfully Spec 2 3');
+    log.logInfo('should update data successfully Spec 2 4');
+    log.logInfo('should update data successfully Spec 2 5');
+    expect(3).toEqual(323);
+  });
 });
